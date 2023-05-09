30b994f665ee8f964264cf0e05ed390547b23f57
chrishart0 <christianphart0@gmail.com>
2023-05-09 19:22:11 -0400

    Add functionality to create markdown for all news items

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aws-news-generator
+
+Use ChatGPT to take an RSS feed and summary the news items into an easy to read article.
--- backend/hello_world/app.py
+++ backend/hello_world/app.py
@@ -37,7 +37,7 @@ def html_to_text(html):
     soup = BeautifulSoup(html, "html.parser")
     return soup.get_text()
 
-def get_feed_items(url, news_items):
+def get_feed_items(url, news_items, markdown_total_news_items):
     # Setup logger
     logger = setup_logger()
 
@@ -48,76 +48,101 @@ def get_feed_items(url, news_items):
     # print("Feed Description:", feed.feed.description)
     print("\n")
 
-    n = 0
     for entry in feed.entries:
         if within_last_7_days(entry.published):
-            news_items[n] = {}
+            entry_data = {}
 
             # print(entry)
             # print(entry.keys())
 
             # Extract data from item and save to news_items with other feed data
-            news_items[n]['title'] = entry.title
+            entry_data['title'] = entry.title
             print("Title:", entry.title)
 
             # if 'title_detail' in entry:
-            #     news_items[n]['title_detail'] = entry.title_detail
+            #     entry_data['title_detail'] = entry.title_detail
             #     print("title_detail:", entry.title_detail)
 
-            news_items[n]['link'] = entry.link
+            entry_data['link'] = entry.link
             print("Link:", entry.link)
 
-            news_items[n]['published'] = entry.published
-            print("Published Date:", entry.published)
+            # Make markdown title with link
+            markdown_total_news_items += f"### [{entry.title}]({entry.link})"
 
-            if 'author' in entry:
-                news_items[n]['author'] = entry.author
-                # print("Author:", entry.author)
+            # entry_data['published'] = entry.published
+            # print("Published Date:", entry.published)
+
+            # if 'author' in entry:
+            #     entry_data['author'] = entry.author
+            #     # print("Author:", entry.author)
 
             # Just another time stamp 
             # if 'published_parsed' in entry:
-            #     news_items[n]['published_parsed'] = entry.published_parsed
+            #     entry_data['published_parsed'] = entry.published_parsed
             #     print("published_parsed:", entry.published_parsed)
 
-            if 'tags' in entry:
-                news_items[n]['tags'] = entry.tags[0]['term']
-                print("tags:", news_items[n]['tags'])
+            # if 'tags' in entry:
+            #     entry_data['tags'] = entry.tags[0]['term']
+            #     print("tags:", entry_data['tags'])
 
-            news_items[n]['summary'] = html_to_text(entry.summary)
-            print("Summary:", news_items[n]['summary'])
+            # entry_data['summary'] = html_to_text(entry.summary)
+            # print("Summary:", entry_data['summary'])
+            markdown_total_news_items += f"\n{html_to_text(entry.summary)}"
             
 
-            if 'content' in entry:
-                news_items[n]['content'] = html_to_text(entry.content[0].value)
-                # print("Content:", html_to_text(entry.content[0].value)) #ToDo: Check if more than one content
+            # if 'content' in entry:
+            #     entry_data['content'] = html_to_text(entry.content[0].value)
+            #     # print("Content:", html_to_text(entry.content[0].value)) #ToDo: Check if more than one content
             print("\n")
 
-            n += 1
-
+            news_items.append(entry_data)
+            markdown_total_news_items += """
+"""
+            print(markdown_total_news_items)
 
 def lambda_handler(event, context):
 
     # Prep storage for news items across feeds
-    news_items = {}
-
+    news_items = []
     blog_feeds = ('https://aws.amazon.com/blogs/aws/feed/', 'https://aws.amazon.com/blogs/architecture/feed/', 'https://aws.amazon.com/blogs/aws-cost-management/feed/', 'https://aws.amazon.com/blogs/apn/feed/', 'https://aws.amazon.com/podcasts/aws-podcast/', 'https://aws.amazon.com/blogs/awsmarketplace/feed/', 'https://aws.amazon.com/blogs/big-data/feed/', 'https://aws.amazon.com/blogs/business-productivity/feed/', 'https://aws.amazon.com/blogs/compute/feed/', 'https://aws.amazon.com/blogs/contact-center/feed/', 'https://aws.amazon.com/blogs/containers/feed/', 'https://aws.amazon.com/blogs/database/feed/', 'https://aws.amazon.com/blogs/desktop-and-application-streaming/feed/', 'https://aws.amazon.com/blogs/developer/feed/', 'https://aws.amazon.com/blogs/devops/feed/', 'https://aws.amazon.com/blogs/enterprise-strategy/feed/', 'https://aws.amazon.com/blogs/mobile/feed/', 'https://aws.amazon.com/blogs/gametech/feed/', 'https://aws.amazon.com/blogs/hpc/feed/', 'https://aws.amazon.com/blogs/infrastructure-and-automation/feed/', 'https://aws.amazon.com/blogs/industries/feed/', 'https://aws.amazon.com/blogs/iot/feed/', 'https://aws.amazon.com/blogs/machine-learning/feed/', 'https://aws.amazon.com/blogs/mt/feed/', 'https://aws.amazon.com/blogs/media/feed/', 'https://aws.amazon.com/blogs/messaging-and-targeting/feed/', 'https://aws.amazon.com/blogs/networking-and-content-delivery/feed/', 'https://aws.amazon.com/blogs/opensource/feed/', 'https://aws.amazon.com/blogs/publicsector/feed/', 'https://aws.amazon.com/blogs/quantum-computing/feed/', 'https://aws.amazon.com/blogs/robotics/feed/', 'https://aws.amazon.com/blogs/awsforsap/feed/', 'https://aws.amazon.com/blogs/security/feed/', 'https://aws.amazon.com/blogs/startups/feed/', 'https://aws.amazon.com/blogs/storage/feed/', 'https://aws.amazon.com/blogs/training-and-certification/feed/', 'https://aws.amazon.com/blogs/modernizing-with-aws/feed/')
-    news_feeds = ('https://aws.amazon.com/about-aws/whats-new/recent/feed/', 'https://aws.amazon.com/blogs/aws/feed/')
+    news_feeds = ('https://aws.amazon.com/about-aws/whats-new/recent/feed/',)
+
+    # Create empty string to hold markdown in for new s
+    markdown_total_news_items = """
+"""
 
     # Process items in feeds
     for feed in news_feeds:
         print(f"Processing: {feed}")
-        get_feed_items(feed, news_items)
+        get_feed_items(feed, news_items, markdown_total_news_items)
+
+    # print(news_items)
+    print()
 
     # Ask AI to make summary
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=f"Convert this list of AWS news items from an RSS feed into a weekly update for AWS users:\n\n{news_items}",
-        temperature=0,
-        max_tokens=64,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
+    # WARNING: ChatGPT can't take input this long. Need to figure out what to do about this.
+    # completion = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo", 
+    #     messages = [
+    #         {"role": "system", "content" : "You are the writer of a summary of AWS News every week, given all items in the AWS news feed. You write in markdown. Start with a few paragraph summary of the biggest updates then write out all the updates with markdown links. Include all region names when mentioned."},
+    #         {"role": "user", "content" : f"{news_items}"}
+    #     ]
+    # )
 
-    print("Chat GPT Summary")
-    print(response)
+    # print(completion)
+    # print('---------------------')
+    # ai_summary = completion['choices'][0]['message']['content']
+    # print(ai_summary)
+
+    # Output markdown news items
+    print("Markdown news items:")
+    print(markdown_total_news_items)
+
+# Prompts put into Chat GPT4 UI
+# I want you to act as a developer and DevOps engineer who uses agile. I will provide you with the AWS news for the week. You will write an article that provides insightful commentary on the news topics at hand. You should use your own experiences, thoughtfully explain why something is important, back up claims with facts, and discuss potential use cases for new releases. Specifically use examples from your own work experience to apply these new updates. Your article will be written in markdown. Here is the news for the week:
+# : <I paste in the json output from my lambda function>
+# For the article, write a title which summarizes the updates and will get users on sites like Reddit and hacker news to click on it. 
+
+
+
+
